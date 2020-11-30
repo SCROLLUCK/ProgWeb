3decefe55775b62e10e08f0f8967f890d41fb759
SCROLLUCK <llc2@icomp.ufam.edu.br>
2020-11-30 19:06:48 -0400

    ORM adicionado

--- app_vigilantes/config/router.js
+++ app_vigilantes/config/router.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const controller = require('../app/controlers/main')
+const areaController = require('../app/controlers/area')
 const router = express.Router();
 
 router.get('/',         controller.index);
 router.get('/about',    controller.about)
-router.get('/ui',    controller.ui)
-router.get('/game', controller.game)
+router.get('/ui',       controller.ui)
+router.get('/game',     controller.game)
+
+router.get('/area',     areaController.index);
 
 module.exports = router;
---
+++ app_vigilantes/database/migrations/20201130212100-create-user.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        unique: false,
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      senha: {
+        unique: false,
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      cursoId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
